a2ff781155c3f9ea3f08a97e25794e7856f1ba22
Tim Foerster <github@mailserver.1n3t.de>
2017-02-11 13:50:56 +0000

    Introduce sma data logger parser and collector integration
    Signed-off-by: Tim Foerster <github@mailserver.1n3t.de>

--- lib/sma_exporter.rb
+++ lib/sma_exporter.rb
@@ -1,5 +1,99 @@
 require "sma_exporter/version"
+require 'prometheus/client'
+require "sma_exporter/data"
 
 module SmaExporter
-  
+  PROMETHEUS = Prometheus::Client.registry
+
+  class Rack
+    def initialize(app)
+      p "moo"
+      @app = app
+    end
+
+    def call(env)
+      Runner.do!
+      @app.call(env)
+    end
+  end
+ 
+  module Runner
+
+    PC = Prometheus::Client
+
+    DC_POW = PC::Gauge.new(:sma_dc_power_kw, "DC Power")
+    DC_U = PC::Gauge.new(:sma_dc_voltage, "DC Voltage")
+    DC_I = PC::Gauge.new(:sma_dc_current, "DC Current")
+    
+    AC_POW = PC::Gauge.new(:sma_ac_power_kw, "AC Power")
+    AC_U = PC::Gauge.new(:sma_ac_voltage, "AC Voltage")
+    AC_I = PC::Gauge.new(:sma_ac_current, "AC Current")
+
+    DEV_TEMP = PC::Gauge.new(:sma_device_temperature, "SMA device temperature")
+    DEV_STATE = PC::Gauge.new(:sma_device_state, "SMA device state")
+    DEV_SN = PC::Gauge.new(:sma_device_sn, "SMA device serialnumber")
+
+    GRID_STATE = PC::Gauge.new(:sma_grid_state, "SMA grid state")
+    GRID_FREQ = PC::Gauge.new(:sma_grid_freq, "SMA grid state")
+
+    module_function
+
+    def do!
+      set_device!
+      set_grid!
+      set_ac!
+      set_dc!
+    ensure
+      flush!
+    end
+
+    def register!
+      [
+        DC_POW, DC_U, DC_I, 
+        AC_POW, AC_I, AC_U, 
+        DEV_TEMP, DEV_STATE, DEV_SN, 
+        GRID_STATE, GRID_FREQ
+      ].each do |x|
+        PROMETHEUS.register x
+      end
+    end
+
+    def set_device!
+      x = data.device
+      DEV_TEMP.set({},x[:temp])
+      DEV_STATE.set({},x[:state])
+      DEV_SN.set({},x[:sn])
+    end
+    
+    def set_grid!
+      x = data.grid
+      GRID_FREQ.set({},x[:grid])
+      GRID_STATE.set({},x[:state])
+    end
+
+    def set_dc!
+      data.dc.each do |x|
+        DC_POW.set({phase: x[:id]}, x[:power])
+        DC_U.set({phase: x[:id]}, x[:voltage])
+        DC_I.set({phase: x[:id]}, x[:current])
+      end
+    end
+   
+    def set_ac!
+      data.ac.each do |x|
+        AC_POW.set({phase: x[:id]}, x[:power])
+        AC_U.set({phase: x[:id]}, x[:voltage])
+        AC_I.set({phase: x[:id]}, x[:current])
+      end
+    end
+
+    def flush!
+      @data = nil
+    end
+
+    def data
+      @data ||= Data.new(sbfpath: ENV.fetch('SMA_SBFPATH'))
+    end
+
+  end
 end
---
+++ lib/sma_exporter/data.rb
@@ -0,0 +1,58 @@
+require 'shellwords'
+
+module SmaExporter
+  class Data
+
+    include Shellwords
+
+    attr_reader :sbfpath
+
+    def initialize(sbfpath:)
+      @sbfpath = sbfpath
+    end
+
+    def device
+      {
+        state: output[/Device Status\s*:\s*(\S+)/,1].to_s.downcase,
+        sn: output[/Device Name\s*:\s*SN:\s*(\S+)/,1],
+        temp: output[/Device Temperature:[^\d]+(\d+\.\d+)/,1].to_f
+      }
+    end
+
+    def grid
+      { 
+        freq: output[/Grid Freq.[^\d]+(\d+\.\d+)/,1].to_f,
+        state: output[/GridRelay Status\s*:\s*(\S+)/,1].to_s.downcase 
+      }
+    end
+
+    def ac
+      output.scan(
+        /Phase\s(\d+)
+        \sPac[^\d]+(\d+\.\d+)
+        [^\d]+(\d+\.\d+)
+        [^\d]+(\d+\.\d+)/x
+      ).map do |x|
+        { id: x[0].to_i, power: x[1].to_f, 
+          voltage: x[2].to_f, current: x[3].to_f }
+      end
+    end
+
+    def dc
+      output.scan(
+        /String\s(\d+)
+        \sPdc[^\d]+(\d+\.\d+)
+        [^\d]+(\d+\.\d+)
+        [^\d]+(\d+\.\d+)/x
+      ).map do |x|
+        { id: x[0].to_i, power: x[1].to_f, 
+          voltage: x[2].to_f, current: x[3].to_f }
+      end
+    end
+
+    def output
+      @output ||= `#{shellescape(sbfpath)} -nosql -nocsv -loadlive -sp0 -v`
+    end
+
+  end
+end
